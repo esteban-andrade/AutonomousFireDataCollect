e82c1898bfda7911295c56cc628880bbdb74728b
Esteban <esgaanza98@gmail.com>
2021-05-27 20:48:43 +1000

    Patched and Updates

---
+++ Interaction/interaction.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import time
+import signal
+
+
+
+   
+def main():
+
+    visual = 'roslaunch flir_one_node flir_data_vis.launch'
+    record = 'roslaunch flir_one_node flir_data_record.launch'
+
+############## Starts Visualizer to Set Record Change to vis to record ######
+    proc = subprocess.Popen(
+        [record], stdout=subprocess.PIPE,
+        shell=True, preexec_fn=os.setsid)
+
+
+######################
+    time.sleep(20)  # <-- sleep for 12''
+
+
+
+###################terminates##########################
+    os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+   
+
+
+  
+
+if __name__ == "__main__":
+    main()
